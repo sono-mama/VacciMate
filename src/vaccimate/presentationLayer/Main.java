129b4c584edd02b62690b773238667aec04096dd
Oliver Kittan <o_kittan@web.de>
2021-06-24 14:11:32 +0200

    Konsolenmenü erster Versuch

--- src/vaccimate/presentationLayer/Main.java
+++ src/vaccimate/presentationLayer/Main.java
@@ -1,5 +1,9 @@
 package vaccimate.presentationLayer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import vaccimate.auxiliary.Address;
 import vaccimate.auxiliary.Contact;
 import vaccimate.process.Appointment;
@@ -10,7 +14,7 @@ import vaccimate.users.Patient;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NumberFormatException, IOException {
 	// write your code here
 
 
@@ -59,8 +63,55 @@ public class Main {
         Address pat1Add = new Address("Musterstr.", "1", "12151", "Berlin");
         Contact pat1Cont = new Contact("03032590909", "01764589901", "max.mustermann@web.de");
         Patient pat1 = new Patient("Max", "Mustermann", 12345, pat1Add, 89, pat1Cont, true);
-
-
-
+        
+        int menuhelper = 0;
+        do {
+        	System.out.println("0 - Programm beenden\n1 - Anmelden als Patient\n2 - Anmelden als Mitarbeiter\n3 - Anmelden als Arzt");
+        	System.out.println("Bitte Funktion durch Eingabe der passenden Zahl wählen");
+        	BufferedReader din= new BufferedReader(new InputStreamReader(System.in));
+        	int numberuser = Integer.parseInt(din.readLine());
+		
+        	switch (numberuser) {
+        		case 0: menuhelper = 1; break;
+        		case 1: // Patient
+					do {
+						System.out.println("0 - zurück\n1 - Termin buchen\n2 - Termin aufrufen");
+						System.out.println("Bitte Funktion durch Eingabe der passenden Zahl wählen");
+						BufferedReader dinpatient = new BufferedReader(new InputStreamReader(System.in));
+						int numberpatient = Integer.parseInt(dinpatient.readLine());
+						switch (numberpatient) {
+							case 0: menuhelper = 1; break;
+							case 1: // Aufruf setAppointment();
+							case 2: // Aufruf showAppointment();
+						}
+					} while(menuhelper == 0); menuhelper = 0;
+					break;
+				case 2: // Rezeption
+					do {
+						System.out.println("0 - zurück\n1 - Termin aufrufen\n2 - Terminübersicht anzeigen");
+						System.out.println("Bitte Funktion durch Eingabe der passenden Zahl wählen");
+						BufferedReader dinreception = new BufferedReader(new InputStreamReader(System.in));
+						int numberreception = Integer.parseInt(dinreception.readLine());
+						switch (numberreception) {
+							case 0: menuhelper = 1; break;
+							case 1: // Aufruf showAppointment();
+							case 2: // Aufruf showAppointmentList();
+						}
+        			} while(menuhelper == 0); menuhelper = 0;
+        			break;
+				case 3: // Arzt
+					do {
+						System.out.println("0 - zurück\n1 - Patient impfen");
+						System.out.println("Bitte Funktion durch Eingabe der passenden Zahl wählen");
+						BufferedReader dindoctor = new BufferedReader(new InputStreamReader(System.in));
+						int numberdoctor = Integer.parseInt(dindoctor.readLine());
+						switch (numberdoctor) {
+							case 0: menuhelper = 1; break;
+							case 1: // Aufruf setVaccineStatus();
+						}
+					} while(menuhelper == 0); menuhelper = 0;
+					break;
+        	}
+        } while (menuhelper == 0);
     }
 }
